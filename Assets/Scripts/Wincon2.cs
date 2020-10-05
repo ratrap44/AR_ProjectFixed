c618302f1b1dfca7eb663aa12a7dae94cf89706a
ratrap44 <62080086+ratrap44@users.noreply.github.com>
2020-10-05 12:54:57 -0400

    maze2

--- Assets/Scripts/Wincon2.cs
+++ Assets/Scripts/Wincon2.cs
@@ -3,17 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Winconjoystick : MonoBehaviour
+public class Wincon2 : MonoBehaviour
 {
-
-
-    void OnTriggerEnter (Collider other)
+    void OnTriggerEnter(Collider other)
     {
-         if (other.name == "Player")
-            {
+        if (other.name == "Player")
+        {
             SceneManager.LoadScene(6);
             Debug.Log("triggered");
-            }
+        }
     }
-   
 }
